5c9cb01f66f52dd4e185301911f79d5748ebf89e
Matthew Richtmyer <mrichtmyer94@gmail.com>
2020-04-01 19:37:08 -0400

    refactor simplifies code and correctly outputs JSON format needed for JS

--- app/app.py
+++ app/app.py
@@ -9,98 +9,42 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/CPG"
 # :5432 cut after localhost
+
+# connect to SQLAlchemy
 db = SQLAlchemy(app)
 
 # create engine
 engine = create_engine("postgresql://postgres:postgres@localhost/CPG")
-# connect to engine
-conn = engine.connect()
-# use pd read_sql to connect to sample table
-data = pd.read_sql("SELECT * FROM eucerin_intensive_lotion",conn) # refactor this: should be part of etl - add engine,conn as inputs
 
 ## Consider making model package - for loading help with speed
 
-#db = create_engine("postgresql://postgres:postgres@localhost:5432/CPG")
-class Reviews(db.Model):
-    __tablename__ ="eucerin_intensive_lotion"
-
-    id = db.Column(db.Integer, primary_key=True)
-    profile_name = db.Column(db.Text)
-    stars = db.Column(db.String(20))
-    title = db.Column(db.Text)
-    review_date = db.Column(db.String(100))
-    review = db.Column(db.Text)
-    helpful = db.Column(db.String(50))
-    form = db.Column(db.String(10))
-    brand = db.Column(db.String(20))
-    sku = db.Column(db.Text)
-    url = db.Column(db.Text)
-
-
-    def __init__(self,profile_name,stars,title,review_date,review,helpful,form,brand,sku,url):
-        self.profile_name = profile_name
-        self.stars = stars
-        self.title = title
-        self.review_date = review_date
-        self.review = review
-        self.helpful = helpful
-        self.form = form
-        self.brand = brand
-        self.sku = sku
-        self.url = url
     
-    def __repr__(self):
-        return 'Brand: {} and rating {}'.format(self.brand, self.stars)
-    
-
-
 @app.route("/")
 def display_reviews():
     return render_template("index.html")
 
 
-@app.route("/emotions")
-def emotion():
+# @app.route("/emotions")
+# def emotion():
 
-    # create engine
-    engine = create_engine("postgresql://postgres:postgres@localhost/CPG")
-    # connect to engine
-    conn = engine.connect()
-    # use pd read_sql to connect to sample table
-    data = pd.read_sql("SELECT * FROM eucerin_intensive_lotion",conn)
+#     # create engine
+#     engine = create_engine("postgresql://postgres:postgres@localhost/CPG")
+#     # connect to engine
+#     conn = engine.connect()
+#     # use pd read_sql to connect to sample table
+#     data = pd.read_sql("SELECT * FROM eucerin_intensive_lotion",conn)
 
-    data = etl.etl(data)
-    emotions = etl.monthlyEmotionAvg(data)
+#     data = etl.etl(data)
+#     emotions = etl.monthlyEmotionAvg(data)
 
-    return jsonify(emotions)
+#     return jsonify(emotions)
 
 
-# @app.route("/ratings")
-# def ratings():
-#     # clean data
-#     d = etl.etl(data)
-
-#     # groupby
-#     g = data.groupby('YearMonth')["stars"].mean()
-
-#     # populate dictionary
-#     # ===================   
-#     ratings = {}
-#     ratings["date"] = g.index.tolist()
-#     ratings["avg_monthly"] = list(g) # mean rating over time grouped by month
-#     ratings["histogram_values"] = np.histogram(data["stars"], bins=[1,2,3,4,5,6])[0].tolist()
-#     ratings["histogram_bins"] = np.histogram(data["stars"], bins=[1,2,3,4,5,6])[1].tolist()
-#     ratings["most_helpful"] = data.iloc[data["helpful"].argmax(),5]
-#     return jsonify(ratings)
 
 @app.route("/ratings")
 def ratings():
-    # clean data
-    d = etl.etl(data)
-    # transformation
-    gb = etl.gbReview(d) # refactor with better function names e.g. data = load_transform()
-
-    return jsonify(gb)
+    data, ratings_dict = etl.read_transform() # will want to eventually pass in table name for queries
+    return jsonify(ratings_dict)
 
 
 @app.errorhandler(404)
--- app/etl.py
+++ app/etl.py
@@ -1,188 +1,171 @@
+from sqlalchemy import create_engine
+
 import pandas as pd
-from nltk.tokenize import word_tokenize
-from nltk.stem.snowball import SnowballStemmer
-import json
 import numpy as np
 
-## Lambda functions ##
-## ---------------- ##
+from nltk.tokenize import word_tokenize
+from nltk.stem.snowball import SnowballStemmer
 
-def star(rev):
-    try:
-        num = int(rev[0])
-        return num
-    except:
-        None
+import json
 
-def helpful(rev):
+        
+
+
+
+def extractStars(row):
+    """Lambda function to extract number of stars left in rating"""
+    return float(row.split(' ')[0]) 
+
+def extractHelpful(row):
+    """Lambda function to extract number of upvotes on Amazon"""
+    rev = row.split(' ')[0]
     
-    try:
-        rev = rev.split(' ')[0]
-        # the first element of the list is a number and we can directly return it
-        num = int(rev)
-        return num
+    if(rev.isnumeric()):
+        return int(rev)
+    elif(rev=="one"):
+        return 1
+    else:
+        return 0
     
-    except:
-        # the first element of the list is the string 'one'
-        if rev == "One":
-            return 1
-        # the list contains 'report abuse' which means there were no upvotes for
-        # this review
-        else:
-            return 0
-
-
-def date(rev):
-    date = pd.to_datetime(rev[33:])
+def extractDate(row):
+    """Lambda function to convert string into datetime object"""
+    date = pd.to_datetime(row[33:])
     return date
 
-def convTime(rev):
-    corr_date = rev-pd.offsets.MonthBegin(1) #rev is a string
+def convertTime(rev):
+    """Lambda function to abstract datetime object per month for groupby"""
+    corr_date = rev-pd.offsets.MonthBegin(1) 
     return corr_date
 
-
-def word_count(rev):
-    # tokenize
+def countWords(rev):
+    """Lambda function to count all words in a particular review"""
     return len(word_tokenize(rev))
 
 
 
-
-# def star(rev):
-#     try:
-#         num = int(rev[0])
-#         return num
-#     except:
-#         None
+# define function to read data from PostgreSQL server
+def readData(table="eucerin_intensive_lotion", 
+         engine=create_engine("postgresql://postgres:postgres@localhost/CPG")):
     
-# def helpful(rev):
-#     try:
-#         rev = rev.split(' ')[0]
-#         # the first element of the list is a number and we can directly return it
-#         num = int(rev)
-#         return num
+    # connect engine
+    conn = engine.connect()
     
-#     except:
-#         # the first element of the list is the string 'one'
-#         if rev == "One":
-#             return 1
-#         # the list contains 'report abuse' which means there were no upvotes for
-#         # this review
-#         else:
-#             return 0
-#         None
-
-# def date(rev):
-#     try:
-#         abc = pd.to_datetime(rev[33:])
-#         return abc
-#     except:
-#         None
-
-# def word_count(rev):
-#     # tokenize
-#     return len(word_tokenize(rev))
-
-
-# def convTime(rev):
-#     try:
-#         corr_date = rev-pd.offsets.MonthBegin(1)
-#         return corr_date
-#     except:
-#         None
-
-def etl(data):
-    d = {}
-    # clean up dataframe with lambda functions defined above
-    d["stars"] = list(data.apply(lambda x: star(x["stars"]), axis=1))
-    d["helpful"] = list(data.apply(lambda x: helpful(x["helpful"]),axis=1))
-    d["review_date"] = list(data.apply(lambda x: date(x["review_date"]), axis=1))
-    d["word_count"] = list(data.apply(lambda x: word_count(x["review"]),axis=1))
-    d['YearMonth'] = list(data.apply(lambda x: convTime(x["review_date"]),axis=1))
-
-    #data['YearMonth'] = data['review_date'] - pd.offsets.MonthBegin(1)
-
-    # pandas df
-    return pd.DataFrame(d)
-
-
-def gbReview(data):
-        # groupby
-    g = data.groupby('YearMonth')["stars"].mean()
-
-    # populate dictionary
-    # ===================   
-    ratings = {}
-    ratings["date"] = g.index.tolist()
-    ratings["avg_monthly"] = list(g) # mean rating over time grouped by month
-    ratings["histogram_values"] = np.histogram(data["stars"], bins=[1,2,3,4,5,6])[0].tolist()
-    ratings["histogram_bins"] = np.histogram(data["stars"], bins=[1,2,3,4,5,6])[1].tolist()
-    #ratings["most_helpful"] = data.iloc[data["helpful"].argmax(),5] 
+    # try making query asked for
+    try:
+        query = f"SELECT * FROM {table}"
+        # attempt to read table queried
+        data = pd.read_sql(query,conn)
+    except:
+        # output default data
+        query = "SELECT * FROM eucerin_intensive_lotion"
+        data = pd.read_sql(query,conn)
     
-    return ratings # return dict
+    return data
 
-def text_emotion(df,column):
+
+# perform data loading and transforming in one function
+def read_transform(table="eucerin_intensive_lotion",
+                   engine=create_engine("postgresql://postgres:postgres@localhost/CPG")):
+    """Docstring: makes query to PostgreSQL database using the table defined.
+    Performs all transformations, including cleaning prior to returning dataframe"""
     
-    new_df = df.copy()
+    # read in raw data from PostgreSQL
+    data = readData(table,engine)
     
-    filepath = ('data/NRC-Sentiment-Emotion-Lexicons/NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt')  
+    # transformations
+    data["stars"] = data.apply(lambda x: extractStars(x["stars"]),axis=1)
+    data["helpful"] = data.apply(lambda x: extractHelpful(x["helpful"]),axis=1)
+    data["review_date"] = data.apply(lambda x: extractDate(x["review_date"]),axis=1)
+    data["corr_date"] = data.apply(lambda x: convertTime(x["review_date"]),axis=1)
+    data["word_count"] = data.apply(lambda x: countWords(x["review"]),axis=1)
     
-    emolex_df = pd.read_csv(filepath, names=["word", "emotion", "association"], sep='\t')
-    emolex_words = emolex_df.pivot(index='word',
-                                   columns='emotion',
-                                   values='association').reset_index()
-    emotions = emolex_words.columns.drop('word')
+    # perform groupby on month to get aggregate data
+    gb = data.groupby('corr_date')["stars"].mean()
     
-    emo_df = pd.DataFrame(0,index=df.index, columns=emotions)
+    # populate dictionary containing all data to pass back to route
+    ratings_dict = {}
+    ratings_dict["review_date"] = list(data["review_date"])
+    ratings_dict["gb_date"] = gb.index.tolist()
+    ratings_dict["avg_monthly_rating"] = list(gb)
+    ratings_dict["histogram_rating_values"] = np.histogram(data["stars"], bins=[1,2,3,4,5,6])[0].tolist()
+    ratings_dict["histogram_rating_bins"] = np.histogram(data["stars"], bins=[1,2,3,4,5,6])[1].tolist()
     
-    stemmer = SnowballStemmer("english")
     
-    for i in range(0, len(new_df)):
-        row = new_df[column][i]
-        document = word_tokenize(row)
+    return data, ratings_dict
+
+
+
+
+
+
+
+
+
+
+
+
+# def text_emotion(df,column):
+    
+#     new_df = df.copy()
+    
+#     filepath = ('data/NRC-Sentiment-Emotion-Lexicons/NRC-Emotion-Lexicon-v0.92/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt')  
+    
+#     emolex_df = pd.read_csv(filepath, names=["word", "emotion", "association"], sep='\t')
+#     emolex_words = emolex_df.pivot(index='word',
+#                                    columns='emotion',
+#                                    values='association').reset_index()
+#     emotions = emolex_words.columns.drop('word')
+    
+#     emo_df = pd.DataFrame(0,index=df.index, columns=emotions)
+    
+#     stemmer = SnowballStemmer("english")
+    
+#     for i in range(0, len(new_df)):
+#         row = new_df[column][i]
+#         document = word_tokenize(row)
         
-        for word in document:
-            word = stemmer.stem(word.lower())
+#         for word in document:
+#             word = stemmer.stem(word.lower())
             
-            emo_score = emolex_words[emolex_words.word == word]
+#             emo_score = emolex_words[emolex_words.word == word]
             
-            if not emo_score.empty:
-                for emotion in list(emotions):
-                    emo_df.at[i,emotion] += emo_score[emotion]
-        new_df = pd.concat([new_df, emo_df], axis=1)
+#             if not emo_score.empty:
+#                 for emotion in list(emotions):
+#                     emo_df.at[i,emotion] += emo_score[emotion]
+#         new_df = pd.concat([new_df, emo_df], axis=1)
     
-    return new_df
+#     return new_df
 
-def monthlyEmotionAvg(df):
-    # calculate date
-    df['YearMonth'] = df['review_date'] - pd.offsets.MonthBegin(1)
-    date = list(df["YearMonth"]) ## HAVE TO HAVE THIS FCN
+# def monthlyEmotionAvg(df):
+#     # calculate date
+#     df['YearMonth'] = df['review_date'] - pd.offsets.MonthBegin(1)
+#     date = list(df["YearMonth"]) ## HAVE TO HAVE THIS FCN
     
-    # calculate emotional response
-    emotion_df = text_emotion(df,"review")
+#     # calculate emotional response
+#     emotion_df = text_emotion(df,"review")
     
-    # take row sums
-    emotion_df["anger_sum"] = emotion_df["anger"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["anticipation_sum"] = emotion_df["anticipation"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["disgust_sum"] = emotion_df["disgust"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["fear_sum"] = emotion_df["fear"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["joy_sum"] = emotion_df["joy"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["negative_sum"] = emotion_df["negative"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["positive_sum"] = emotion_df["positive"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["sadness_sum"] = emotion_df["sadness"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["surprise_sum"] = emotion_df["surprise"].sum(axis=1)/len(emotion_df["review"])
-    emotion_df["trust_sum"] = emotion_df["trust"].sum(axis=1)/len(emotion_df["review"])
+#     # take row sums
+#     emotion_df["anger_sum"] = emotion_df["anger"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["anticipation_sum"] = emotion_df["anticipation"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["disgust_sum"] = emotion_df["disgust"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["fear_sum"] = emotion_df["fear"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["joy_sum"] = emotion_df["joy"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["negative_sum"] = emotion_df["negative"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["positive_sum"] = emotion_df["positive"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["sadness_sum"] = emotion_df["sadness"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["surprise_sum"] = emotion_df["surprise"].sum(axis=1)/len(emotion_df["review"])
+#     emotion_df["trust_sum"] = emotion_df["trust"].sum(axis=1)/len(emotion_df["review"])
     
-    # take just sums
-    emotion_df = emotion_df.iloc[:,-10:]
+#     # take just sums
+#     emotion_df = emotion_df.iloc[:,-10:]
     
-    # store emotional response for all 10 vectors in dictionary
-    month_avg = {}
-    for col in emotion_df.columns:
-        emotions = list(emotion_df[col])
-        col_name = col[:-4]
-        # pd dataframe is not json serializable
-        month_avg[col_name] = list(pd.DataFrame({"Date": date, "Emotion": emotions}).groupby("Date").mean()["Emotion"])#.plot(kind="line") 
+#     # store emotional response for all 10 vectors in dictionary
+#     month_avg = {}
+#     for col in emotion_df.columns:
+#         emotions = list(emotion_df[col])
+#         col_name = col[:-4]
+#         # pd dataframe is not json serializable
+#         month_avg[col_name] = list(pd.DataFrame({"Date": date, "Emotion": emotions}).groupby("Date").mean()["Emotion"])#.plot(kind="line") 
     
-    return json.dumps(month_avg) # this is just returning a dictionary, not list of dictionary which is needed for JS
+#     return json.dumps(month_avg) # this is just returning a dictionary, not list of dictionary which is needed for JS
 
